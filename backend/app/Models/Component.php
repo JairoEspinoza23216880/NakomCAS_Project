8ec56e1168f0204e8ac41f87d6464e46a687ea62
a23216880@alumnos.uady.mx <a23216880@alumnos.uady.mx>
2025-11-22 11:30:39 -0600

    Corrección de Errores Menores

--- backend/app/Models/Component.php
+++ backend/app/Models/Component.php
@@ -12,12 +12,14 @@ class Component extends Model
         'name',
         'description',
         'price',
-        'stock_quantity'
+        'stock_quantity',
+        'component_type_id',
+        'tier'
     ];
 
     public function orders()
     {
-        return $this->belongsToMany(Order::class, 'order_components')
-            ->withPivot('quantity', 'price_at_order');
+        return $this->belongsToMany(Order::class, 'order_x_components')
+            ->withPivot('quantity', 'price_at_purchase');
     }
 }
