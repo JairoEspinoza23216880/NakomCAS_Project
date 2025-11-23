b326417eeca45dba21df6e1018270ee3fddc6e86
JairoEspinoza23216880 <142467514+JairoEspinoza23216880@users.noreply.github.com>
2025-11-23 10:48:05 -0600

    Archivos de Modelos corregidos y estructurados (#23)

--- backend/app/Models/Component.php
+++ backend/app/Models/Component.php
@@ -6,17 +6,26 @@ use Illuminate\Database\Eloquent\Model;
 
 class Component extends Model
 {
+    // TABLA
     protected $table = 'components';
 
+
+    // ATRIBUTOS
+    // Asignables
     protected $fillable = [
         'name',
-        'description',
         'price',
-        'stock_quantity',
+        'stock',
         'component_type_id',
         'tier'
     ];
 
+    // Desactivar timestamps
+    public $timestamps = false;
+
+
+    // RELACIONES
+    // Varios Componentes pertenecen varias Órdenes
     public function orders()
     {
         return $this->belongsToMany(Order::class, 'order_x_components')
