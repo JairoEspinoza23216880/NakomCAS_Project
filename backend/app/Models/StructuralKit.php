1b0c3a9e2aa83da474a0cf8435d649c0d51db4e7
LUIS <142747436+IANT9@users.noreply.github.com>
2025-11-22 21:50:52 -0600

    Define StructuralKit model properties and relationships
    Added table and fillable properties, and defined components relationship.

--- backend/app/Models/StructuralKit.php
+++ backend/app/Models/StructuralKit.php
@@ -1,10 +1,20 @@
 <?php
-
 namespace App\Models;
-
 use Illuminate\Database\Eloquent\Model;
 
 class StructuralKit extends Model
 {
-    // Aquí va el código del modelo StructuralKit
+    protected $table = 'structural_kits';
+    protected $fillable = ['name', 'structural_price', 'case_tier', 'status'];
+    
+    // Relación: Muchos a Muchos con Componentes
+    public function components()
+    {
+        return $this->belongsToMany(
+            Component::class, 
+            'structural_kits_x_components', 
+            'structural_kit_id', // Esta clave
+            'component_id'
+        )->withPivot('quantity');
+    }
 }
