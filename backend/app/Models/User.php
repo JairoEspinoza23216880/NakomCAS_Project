53652367ac1bea7220b93631bd0c377e51c9935f
a23216880@alumnos.uady.mx <a23216880@alumnos.uady.mx>
2025-11-22 20:46:28 -0600

    /api/register creado

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -6,15 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class User extends Model
 {
+    // Nombre de la tabla en la base de datos
     protected $table = 'users';
 
+    // Campos asignables masivamente
     protected $fillable = [
         'name',
         'lastname',
         'email',
         'password',
-        'user_role_id'
+        'user_role_id',
+        'phone_number',
+        'status'
     ];
+
+    // Desactivar timestamps si no se usan
+    public $timestamps = false;
 
     public function orders()
     {
